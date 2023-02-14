40bd2f64be87fa61217a547ea8ef2b4317ec1b81
Covadonga M <cova.millan@gmail.com>
2023-02-14 18:50:37 +0100

    Update test made by Joseba

--- src/features/card/component/card.test.tsx
+++ src/features/card/component/card.test.tsx
@@ -6,7 +6,7 @@ describe("Given the Card component", () => {
   describe("When it's rendered", () => {
     test("Then it should contain cards with heading elements", () => {
       const names = screen.getAllByRole("heading");
-      const namesLength = 2;
+      const namesLength = names.length;
       expect(names.length).toBe(namesLength);
       expect(names[0]).toBeInTheDocument();
     });
