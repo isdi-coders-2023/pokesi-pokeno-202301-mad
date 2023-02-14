ee28d35b27220d904f2f99726e5d2f8431ec162a
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-14 15:34:18 +0100

    Add card mocks

---
+++ src/feature/card/mocks/card-data.ts
@@ -0,0 +1,14 @@
+export const POKEMOCK = [
+  {
+    name: "charmander",
+    types: ["fire"],
+    sprite: "../../../../assets/card/4.png",
+    isFavorite: false,
+  },
+  {
+    name: "charmander",
+    types: ["fire", "fire"],
+    sprite: "../../../../assets/card/4.png",
+    isFavorite: true,
+  },
+];
