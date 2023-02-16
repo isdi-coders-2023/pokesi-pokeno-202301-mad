2951fb39dd58a27bcfb7e2d422b767c397cf9664
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-16 23:38:20 +0100

    Add ApiRepo tests

--- src/services/repository/poke.api.repo.test.ts
+++ src/services/repository/poke.api.repo.test.ts
@@ -1,18 +1,23 @@
-import { LoadPokeStructure, PokeStructure } from "../../models/poke";
+import {
+  InfoStructure,
+  LoadPokeStructure,
+  PokeStructure,
+} from "../../models/poke";
 import { PokeApiRepo } from "./poke.api.repo";
 
 global.fetch = jest.fn().mockResolvedValue({});
 
-const mockResults = [
-  { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
-];
+const mockTypes: InfoStructure = { results: [{ name: "grass" }] };
+
+const mockRegions: InfoStructure = { results: [{ name: "kanto" }] };
+
 let repo: PokeApiRepo;
 
-const mock = {
+const mockFetch = {
   count: 2,
   next: "",
   previous: "",
-  results: mockResults,
+  results: { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
 };
 
 describe("Given the PokeApiRepo", () => {
@@ -20,20 +25,30 @@ describe("Given the PokeApiRepo", () => {
     repo = new PokeApiRepo();
   });
   describe("When LoadPokemons is called", () => {
-    test("Then some", async () => {
+    test("Then the returned value should be an iterated object", async () => {
       global.fetch = jest.fn().mockResolvedValue({
-        json: jest.fn().mockResolvedValue(mock),
+        json: jest.fn().mockResolvedValue(mockFetch),
       });
-      const result =
-        (await repo.loadPokemons()) as unknown as LoadPokeStructure;
-      expect(result).toEqual(Object(result));
+      const load = (await repo.loadPokemons()) as unknown as LoadPokeStructure;
+      expect(load).toEqual(Object(load));
     });
   });
   describe("When loadTypes is called", () => {
-    test("Then some 2", () => {
+    test("Then the returned value should be grass", async () => {
       global.fetch = jest.fn().mockResolvedValue({
-        json: jest.fn().mockResolvedValue(mock),
+        json: jest.fn().mockResolvedValue(mockTypes),
       });
+      const types = await repo.loadTypes();
+      expect(types).toEqual(["grass"]);
+    });
+  });
+  describe("When the loadRegions", () => {
+    test("Then the returned value should be kanto", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue(mockRegions),
+      });
+      const regions = await repo.loadRegions();
+      expect(regions).toEqual(["kanto"]);
     });
   });
 });
