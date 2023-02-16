5967e1351bfbb1effe9c8a55ea1674ba79bd46c6
Covadonga M <cova.millan@gmail.com>
2023-02-16 16:25:42 +0100

    Fix error in menu test

--- src/core/components/menu/menu.test.tsx
+++ src/core/components/menu/menu.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import { menuOptions } from "../app/App";
 import { Menu } from "./menu";
 
 describe("Given menu component", () => {
-  render(<Menu></Menu>);
+  render(<Menu options={menuOptions}></Menu>);
   describe("When its rendered", () => {
     test("Then it should render home as a list element", () => {
       const listElement = screen.getByText(/home/i);
