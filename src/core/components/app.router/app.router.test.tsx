8446f97de37cf77e4e7c1754da36624fa139bf93
Covadonga M <cova.millan@gmail.com>
2023-02-16 16:21:00 +0100

    Add app router component

---
+++ src/core/components/app.router/app.router.test.tsx
@@ -0,0 +1,36 @@
+import { MenuOption } from "../app/App";
+import { AppRouter } from "./app.router";
+import { MemoryRouter as Router } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+
+const mockOptions: MenuOption[] = [
+  { label: "Home", path: "/home" },
+  { label: "Favorites", path: "/favorites" },
+  { label: "Create a trainer", path: "/create-trainer" },
+  { label: "Your trainer", path: "/your-trainer" },
+];
+
+describe("Given the App router component", () => {
+  describe("When you render the different paths", () => {
+    test("Then it should render", async () => {
+      render(
+        <Router
+          initialEntries={[
+            "/",
+            "/home",
+            "/favorites",
+            "/create-trainer",
+            "/your-trainer",
+          ]}
+          initialIndex={0}
+        >
+          <AppRouter menuOptions={mockOptions}></AppRouter>
+        </Router>
+      );
+      const elements = await screen.findAllByRole("heading", {
+        name: "CHARMANDER",
+      });
+      expect(elements[0]).toBeInTheDocument();
+    });
+  });
+});
--- src/core/components/app.router/app.router.tsx
+++ src/core/components/app.router/app.router.tsx
@@ -1,14 +1,13 @@
 import { lazy, Suspense } from "react";
 
 import { MenuOption } from "../app/App";
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Error } from "../error/error";
-import { Home } from "../home/home";
 
-const Home = lazy (() => import('../home/home'))
-const Favorites = lazy (() => import ())
-const CreateTrainer = lazy (() => import())
-const YourTrainer = lazy (() => import())
+const Home = lazy(() => import("../home/home"));
+// const Favorites = lazy (() => import ())
+// const CreateTrainer = lazy (() => import())
+// const YourTrainer = lazy (() => import())
 
 type AppRouterProps = {
   menuOptions: MenuOption[];
@@ -18,23 +17,15 @@ export function AppRouter({ menuOptions }: AppRouterProps) {
   return (
     <Suspense>
       <Routes>
-        <Route>
-          path = {"/"} element= {<Home></Home>}
+        <Route path={"/"} element={<Home></Home>}></Route>
+        <Route path={"/home"} element={<Home></Home>}></Route>
+        {/* <Route path={menuOptions[0].path} element={<Favorites></Favorites>}>
         </Route>
-        <Route>
-          path = {menuOptions[0].path} element= {<Favorites></Favorites>}
+        <Route path={menuOptions[1].path} element={<CreateTrainer></CreateTrainer>}>
         </Route>
-        <Route>
-          path = {menuOptions[1].path} element=
-          {<CreateTrainer></CreateTrainer>}
-        </Route>
-        <Route>
-          path = {menuOptions[2].path} element= {<YourTrainer></YourTrainer>}
-        </Route>
-        <Route
-          path={"*"}
-          element={<Error></Error>}
-        ></Route>
+        <Route  path={menuOptions[2].path} element={<YourTrainer></YourTrainer>}>
+        </Route> */}
+        <Route path={"*"} element={<Error></Error>}></Route>
       </Routes>
     </Suspense>
   );
