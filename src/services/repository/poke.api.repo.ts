b11549ddb787437d63845b0d3c400f8da9bd5b62
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-16 14:14:44 +0100

    Add types and regions fetch

--- src/services/repository/poke.api.repo.ts
+++ src/services/repository/poke.api.repo.ts
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-useless-constructor */
-import { PokeStructure, TypeStructure } from "../../models/poke";
+import { PokeStructure, InfoStructure } from "../../models/poke";
 
 export class PokeApiRepo {
   url: string;
   typesUrl: string;
+  regionsUrl: string;
   constructor() {
     this.url = "https://pokeapi.co/api/v2/pokemon/";
     this.typesUrl = "https://pokeapi.co/api/v2/type/";
+    this.regionsUrl = "https://pokeapi.co/api/v2/region/";
   }
 
   async loadPokemons(): Promise<PokeStructure[]> {
@@ -22,11 +24,17 @@ export class PokeApiRepo {
     return pokeData;
   }
 
-  async loadTypes(): Promise<TypeStructure> {
+  async loadTypes(): Promise<string[]> {
     const resp = await fetch(this.typesUrl);
-    const data = (await resp.json()) as TypeStructure;
-    const some = data.results.map((item) => item.name);
-    console.log(some);
-    return data;
+    const data = (await resp.json()) as InfoStructure;
+    const types = data.results.map((item) => item.name);
+    return types.slice(0, 18);
+  }
+
+  async loadRegions(): Promise<string[]> {
+    const resp = await fetch(this.regionsUrl);
+    const data = (await resp.json()) as InfoStructure;
+    const regions = data.results.map((item) => item.name);
+    return regions;
   }
 }
