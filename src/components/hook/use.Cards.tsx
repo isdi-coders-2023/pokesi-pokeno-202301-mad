2eb62cbdf6c60eb6cf3f9ec27c7644fd5563500a
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-17 18:21:23 +0100

    Add hook file

--- src/components/hook/use.Cards.tsx
+++ src/components/hook/use.Cards.tsx
@@ -1,33 +1,35 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useReducer } from "react";
 import { PokeStructure } from "../../models/poke";
 import { PokeApiRepo } from "../../services/repository/poke.api.repo";
+import { cardsReducer } from "../reducer/cards.reducer";
+import * as ac from "../reducer/cards.action.creators";
 
 export type UsePokeStructure = ReturnType<typeof useCards>;
 export function useCards(repo: PokeApiRepo) {
   const initialState: PokeStructure[] = [];
 
-  repo = useMemo(() => new PokeApiRepo(), []);
+  // repo = useMemo(() => new PokeApiRepo(), []);
 
-  const [some, setSome] = useState<PokeStructure[]>(initialState);
-
-  const handleLoad = useCallback(async () => {
-    try {
-      const poke = await repo.loadPokemons();
-      setSome(poke);
-    } catch (error) {
-      handlerError(error as Error);
-    }
-  }, [repo]);
-
-  useEffect(() => {
-    handleLoad();
-  }, [handleLoad]);
+  const [some, dispatch] = useReducer(cardsReducer, initialState);
 
   const handlerError = (error: Error) => {
     console.log(error.message);
   };
 
-  // const load = useCallback(async () => {
+  const loadPokemons = useCallback(async () => {
+    try {
+      const cards = await repo.loadPokemons();
+      dispatch(ac.loadCardsCreator(cards));
+    } catch (error) {
+      handlerError(error as Error);
+    }
+  }, [repo]);
+
+  // useEffect(() => {
+  //   handleLoad();
+  // }, [handleLoad]);
+
+  // const loadCards = useCallback(async () => {
   //   try {
   //     const cards = await repo.loadPokemons();
   //     setCards(cards);
@@ -47,6 +49,6 @@ export function useCards(repo: PokeApiRepo) {
 
   return {
     some,
-    handleLoad,
+    loadPokemons,
   };
 }
