57d01c0ba83d8a9daa7d919e02752093c6ff2c9e
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-18 19:11:21 +0100

    Update cards test

--- src/components/cards/cards.test.tsx
+++ src/components/cards/cards.test.tsx
@@ -1,37 +1,28 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
 import { render, screen } from "@testing-library/react";
-import { PokeStructure } from "../../models/poke";
-import { PokeApiRepo } from "../../services/repository/poke.api.repo";
-import { Card } from "../card/card";
-import { useCards } from "../hook/use.cards";
+import { act } from "react-dom/test-utils";
 import { Cards } from "./cards";
-
-const mocky = {
-  base_experience: 23,
-  height: 12,
-  id: 1,
-  name: "pepe",
-  sprites: { front_default: "" },
-  types: [{ type: { name: "fire" } }],
-  weight: 90,
-  isFavorite: true,
-} as PokeStructure;
-
-beforeEach(() => {
-  render(<Cards></Cards>);
-});
 
 describe("Given the Card component", () => {
   describe("When it's rendered", () => {
     test("Then it should contain cards with heading elements", () => {
+      render(<Cards></Cards>);
       const list = screen.getByRole("list");
       expect(list).toBeInTheDocument();
     });
-  });
-
-  describe("Some", () => {
-    test("QLQ", () => {
-      expect(Card).toHaveBeenCalled();
+    describe("SOME", () => {
+      test("QLQ", async () => {
+        act(() => {
+          render(<Cards></Cards>);
+        });
+        const name = await screen.findByRole(
+          "heading",
+          { name: "CHARMANDER" },
+          { timeout: 2000 }
+        );
+        expect(name).toBeInTheDocument();
+      });
     });
   });
 });
--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -2,10 +2,14 @@ import "./cards.scss";
 import { Card } from "../card/card";
 import { PokeApiRepo } from "../../services/repository/poke.api.repo";
 import { useCards } from "../hook/use.cards";
+import { useEffect } from "react";
 
 export function Cards() {
   const { some, loadPokemons } = useCards(new PokeApiRepo());
-  loadPokemons();
+
+  useEffect(() => {
+    loadPokemons();
+  }, [loadPokemons]);
 
   return (
     <div className="container">
